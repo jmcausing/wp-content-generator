f47c8a38fa760e08367f19edb8f220ab393673c7
JMC <johnmarkcausing@gmail.com>
2020-06-18 21:20:24 +0800

    added html inputs and few css

--- wp-content-generator.php
+++ wp-content-generator.php
@@ -10,6 +10,17 @@ Author URI:  http://causingdesignscom.kinsta.cloud/
 
 
 
+// redirect to plugin option page when plugin is activated
+function wcg_activation_redirect( $plugin ) {
+  if( $plugin == plugin_basename( __FILE__ ) ) {
+      exit( wp_redirect( admin_url( 'admin.php?page=wcg_page' ) ) );
+  }
+}
+add_action( 'activated_plugin', 'wcg_activation_redirect' );
+
+
+
+
 
 
 function wcg_register_options_page() {
@@ -24,38 +35,68 @@ function wcg_register_options_page() {
 function wcg_admin_page() {
 
 
-/*   
-    // Check if file exist then call the js chart file
-    $filename = WP_CONTENT_DIR . "/uploads/ajax-log.json";
-    if(file_exists($filename)){
-      // call the js chart file
-      echo '<script type="text/javascript" src="' . plugin_dir_url(__FILE__) . '/js/charts.js"></script>';
-    }
-    else{
-      echo "<h1>Ajax file has been deleted</h1>";
-    }
-
-
-     */
-  
-  
 
   
-   $html_output = ' <h1> Here are the available post types (registered) </h1>
   
-  
-   ';
-       echo $html_output;
+  // let's get all registered post types
+  global $wp_post_types;
+  // var_dump($wp_post_types);
 
-global $wp_post_types;
-// var_dump($wp_post_types);
+?>
 
-foreach ( $wp_post_types as $type ) { 
 
-    echo $type->name . "<br>";
+<style>
 
+.wcg_inputs {
+  display: inline;
+  padding: 15px;
+  margin: 10px;
 }
 
+</style>
+
+<div class="wcg_container">
+
+  <h1> Here are the available post types (registered) </h1>
 
 
+  <div class="wcg_inputs"> 
+    <input type="number"  min="1" max="55" value="20" class="wcg_count">
+  </div>
+
+  <div class="wcg_inputs">
+    <input type="checkbox" class="wcg_featured_image"> <label> With featured image?</label>
+  </div>
+
+  <div class="wcg_inputs">
+  <!--  Get all registered post types -->
+  <label> Select post type</label>
+    <select name="" id="" class="wgc_post_type">
+        <?php
+          // get all registered post types
+          foreach ( $wp_post_types as $type ) { 
+        ?>
+        <option> <?php echo $type->name ?> </option>
+        <?php } ?>
+    </select>
+  </div>
+
+  <div class="wcg_inputs">
+    <button class="wcg_submit"> Generate content!</button>
+  </div>
+
+
+
+</div>
+  
+  
+   
+
+
+
+
+
+
+
+<?php
 }
