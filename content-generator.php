e148e98c992d706b4a2fe10aa7fcdee30655cdf4
jmcausing <johnmarkcausing@gmail.com>
2020-06-18 19:37:01 +0800

    first commit - wp content generator by jm

---
+++ content-generator.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Content Generator
+Plugin URI: http://causingdesignscom.kinsta.cloud/
+Description: This plugin is to generate content for post types that are currently registered.
+Author: John Mark Causing
+Author URI:  http://causingdesignscom.kinsta.cloud/
+*/
+
